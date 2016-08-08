b6beeb859304067ae8be0d55eea67c5aeaeb00ff
Dmitry Vlasov <vlasov@academ.org>
2016-08-08 12:54:10 +0700

    LFS metamath sources are removed

--- uset-10000.mm
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:17b98604a18c0a8b28c23a5a769b90d6b267918a6c8395ab15fb3d3ac45a4bab
-size 455939
--- uset-100000.mm
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7f5121fa39cf958a39e7b676693315783edc54e4b399667f54e1bdfbfdfc1896
-size 5444230
